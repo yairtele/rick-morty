9c3b5a7626802cf688c4f351ac2fd18963585393
yairtele <ytelezon@est.frba.utn.edu.ar>
2023-10-11 15:53:43 +0200

    First commit. Page with title, image and button that executes a console log. Addition of styles sheet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import imageRickMorty from './img/rick-morty.png'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const reqApi = () => {
+    console.log("Click has been done")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="title">Rick & Morty</h1>
+        <img src={imageRickMorty} alt="Rick & Morty" className="img-home"></img>
+        <button onClick={reqApi} className="btn-search">Search Characters</button>
+      </header> 
     </div>
   );
 }
